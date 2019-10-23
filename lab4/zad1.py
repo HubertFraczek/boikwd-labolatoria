b63a4007c31ed25bf4261cb7b0ae8fd5f8a55f78
HubertFraczek <hhubcio12@gmail.com>
2019-10-23 21:15:45 +0200

    zad2

--- lab4/zad1.py
+++ lab4/zad1.py
@@ -12,18 +12,6 @@ def koczkodaj(m):
                     res = l
     return res
 
-A=[[1,2/3,2,5/2,5/3,5],[
-3/2,1,3,10/3,3,9],[
-1/2,1/3,1,4/3,7/8,5/2],[
-2/5,3/10,3/4,1,5/6,12/5],[
-3/5,1/3,8/7,6/5,1,3],[
-1/5,1/9,2/5,5/12,1/3,1]]
-
-n = 6
-k = 2
-v = 1 - ((1 + math.sqrt(4 * (n - 1) * (n - k - 2))) / (2 * (n - 1)))
-print(koczkodaj(A) < v)
-
 def genA(A, n, k):
     A1 = []
     for i in range(n-k):
@@ -52,89 +40,121 @@ def print_(m):
     for i in range(len(m)):
         print(m[i])
 
-A1, A2 = genA(A, n, k)
-print("A1")
-print_( A1)
-print("A2")
-print_(A2)
+def arithmetic(M, n, k, vec):
+    v = 1 - ((1 + math.sqrt(4 * (n - 1) * (n - k - 2))) / (2 * (n - 1)))
+    if koczkodaj(M) > v:
+        print("koczkodaj nie wyszedl")
+    A1, A2 = genA(M, n, k)
+    print("A1")
+    print_( A1)
+    print("A2")
+    print_(A2)
 
-a = mulA(A1, n)
-b = mulB(A2, n, [[3], [1]])
+    a = mulA(A1, n)
+    b = mulB(A2, n, vec)
 
-print()
-print_(a)
-print()
-print_(b)
+    print()
+    print_(a)
+    print()
+    print_(b)
 
-W = np.dot(np.linalg.inv(a), b)
-W = np.append(W, [[3], [1]])
-print()
-print_(W)
+    W = np.dot(np.linalg.inv(a), b)
+    W = np.append(W, vec)
+    print()
+    print_(W)
 
-# B
+A=[[1,2/3,2,5/2,5/3,5],[
+3/2,1,3,10/3,3,9],[
+1/2,1/3,1,4/3,7/8,5/2],[
+2/5,3/10,3/4,1,5/6,12/5],[
+3/5,1/3,8/7,6/5,1,3],[
+1/5,1/9,2/5,5/12,1/3,1]]
+
 B=[[1,2/5,3,7/3,1/2,1],[
 5/2,1,4/7,5/8,1/3,3],[
 1/3,7/4,1,1/2,2,1/2],[
 3/7,8/5,2,1,4,2],[
 2,3,1/2,1/4,1,1/2],[
 1,1/3,2,1/2,2,1]]
-n = 6
-k = 3
-v = 1 - ((1 + math.sqrt(4 * (n - 1) * (n - k - 2))) / (2 * (n - 1)))
 
-A1, A2 = genA(B, n, k)
-print("#########3 B")
-print("A1")
-print_( A1)
-print("A2")
-print_(A2)
-
-a = mulA(A1, n)
-b = mulB(A2, n, [[2], [1/2], [1]])
-
-print()
-print_(a)
-print()
-print_(b)
-
-W = np.dot(np.linalg.inv(a), b)
-W = np.append(W, [[2], [1/2], [1]])
-print()
-print_(W)
-
-# C
 C=[[1,17/4,17/20,8/5,23/6,8/3],[
 4/17,1,1/5,2/5,9/10,2/3],[
 20/17,5,1,21/10,51/10,10/3],[
 5/8,5/2,10/21,1,5/2,11/6],[
 6/23,10/9,10/51,2/5,1,19/30],[
 3/8,3/2,3/10,6/11,30/19,1]]
-n = 6
-k = 2
-v = 1 - ((1 + math.sqrt(4 * (n - 1) * (n - k - 2))) / (2 * (n - 1)))
 
 C[2], C[4] = C[4], C[2]
 C[4], C[5] = C[5], C[4]
 
-A1, A2 = genA(C, n, k)
-print("#########3 C")
-print("A1")
-print_( A1)
-print("A2")
-print_(A2)
+print("#### średnia arytmetyczna")
+print("zad A")
+arithmetic(A, 6, 2, [[3], [1]])
+print("zad B")
+arithmetic(B, 6, 3, [[2], [1/2], [1]])
+print("zad C")
+arithmetic(C, 6, 2, [[2], [5]])
 
-a = mulA(A1, n)
-b = mulB(A2, n, [[2], [5]])
+def calcB(M, n, k, vec):
+    b = []
+    for i in range(n-k):
+        tmpB = 1
+        for j in range(n):
+            tmpB *= M[i][j]
+        for q in range(k):
+            tmpB *= vec[q][0]
+        b.append(math.log10(tmpB))
+    return b
 
-print()
-print_(a)
-print()
-print_(b)
+def genA(n, k):
+    A = []
+    for i in range(n-k):
+        row = []
+        for j in range(n-k):
+            if i == j:
+                row.append(n-1)
+            else:
+                row.append(-1)
+        A.append(row)
+    return A
 
-W = np.dot(np.linalg.inv(a), b)
-W = np.append(W, [[2], [5]])
-print()
-print_(W)
+def calcW(A, b, vec):
+    W = np.dot(np.linalg.inv(A), b)
+    for i in range(len(W)):
+        tmp = 10**W[i]
+        W[i] = tmp
+    W = np.append(W, vec)
+    return W
 
+def geometric(M, n, k, vec):
+    b = calcB(M, n, k, vec)
+    print("b")
+    print_(b)
+    A = genA(n, k)
+    print("A")
+    print_(A)
+    W = calcW(A, b, vec)
+    print("W")
+    print_(W)
 
+print("#### średnia geometryczna")
+print("zad A")
+geometric(A, 6, 2, [[3], [1]])
+print("zad B")
+geometric(B, 6, 3, [[2], [1/2], [1]])
+print("zad C")
 
+C=[[1,17/4,17/20,8/5,23/6,8/3],[
+4/17,1,1/5,2/5,9/10,2/3],[
+20/17,5,1,21/10,51/10,10/3],[
+5/8,5/2,10/21,1,5/2,11/6],[
+6/23,10/9,10/51,2/5,1,19/30],[
+3/8,3/2,3/10,6/11,30/19,1]]
+
+C = np.array(C)
+C[:, [1, 4]] = C[:, [4, 1]]
+C[:, [3, 5]] = C[:, [5, 3]]
+C[1], C[4] = C[4], C[1]
+C[3], C[5] = C[5], C[3]
+
+geometric(C, 6, 2, [[2], [5]])
